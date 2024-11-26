d1c4350201a2cd0c5689a62af9eb1defe94b225c
Mohsen Zainalpour <147535502+zainalpour-ncl@users.noreply.github.com>
2024-11-25 21:14:22 -0500

    Filter out external services and enrich ServiceCall with the name of the file where the service is used

--- core/src/main/scala/com/ncl/audit/model.scala
+++ core/src/main/scala/com/ncl/audit/model.scala
@@ -5,7 +5,7 @@ case class Service(name: String, methods: Set[RpcMethod], definedIn: Option[Stri
 // TODO: Add support for project dependencies
 // case class ProjectDependency(project: ProjectRef, serviceCalls: Set[ServiceCall])
 case class ProjectDependency(serviceCalls: Set[ServiceCall])
-case class ServiceCall(serviceName: String, calledMethods: Set[MethodCall])
+case class ServiceCall(serviceName: String, calledMethods: Set[MethodCall], calledIn: Option[String] = None)
 case class MethodCall(methodName: String, inputType: Option[String] = None, outputType: Option[String] = None)
 // TODO: Add support for project dependencies
 // case class ProjectRef(name: String, repository: String)
